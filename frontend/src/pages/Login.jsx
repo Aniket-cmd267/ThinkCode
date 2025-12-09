9318e5f37d18625576d97d55f415dcd1c7d32220
Aniket Tiwari <aniketiwari2525@gmail.com>
2025-12-09 23:38:26 +0530

    Signup Login page created with validations using zod react-hook-form

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,46 @@
+import {useForm} from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod' 
+import z from "zod"
+
+const loginSchema= z.object({
+    email: z.email(),
+    password: z.string().min(3,"Password must be atleast 3 characters")
+})
+
 function Login(){
+    
+    const { register, handleSubmit, formState: {errors}}= useForm({resolver: zodResolver(loginSchema)});
+    const submitHandler= (data)=>{
+        console.log(data)
+    }
     return (
-        <>
-            <div className="">Login Page</div>
-        </>
+        <div className='flex justify-center items-center p-4 min-h-screen'>
+            <div className='card w-96 shadow-xl bg-base-100'>
+                <div className='card-body'>
+                    <form onSubmit={handleSubmit(submitHandler)}>
+                        <div className='form-control mt-4'>
+                            <label className="label mb-1">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type='email' placeholder='Enter email' {...register('email')} className='input input-bordered input-md'/>
+                            {errors.email && (<span className='text-error'>{errors.email.message}</span>)}
+                        </div>
+
+                        <div className='form-control mt-4'>
+                            <label className="label mb-1">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type='password' placeholder='Enter password' {...register('password')} className='input input-bordered input-md'/>
+                            {errors.password && (<span className='text-error'>{errors.password.message}</span>)}
+                        </div>
+
+                        <div className='flex justify-center mt-4'>
+                            <button type="submit" className='btn btn-primary py-1 px-5'>Sign Up</button>
+                        </div>   
+                    </form>
+                </div>
+            </div>
+        </div>
     )
 }
 
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,7 +1,70 @@
-import { useState } from "react";
 import { useForm } from 'react-hook-form';
+import { zodResolver } from "@hookform/resolvers/zod"
+import { z } from 'zod';
+
+// schema Validation for signup form
+const signupSchema= z.object({
+    firstName: z.string().min(3, "Name must contain atleast contain 3 characters"),
+    email: z.email("Invalid Email"),
+    password: z.string().min(8,"Password must contain atleast 8 characters")
+})
+
+//  errors= {
+//     firstName : {
+            // type: min,
+            // message: ""
+    // }
+// }
 function Signup(){
-    // const [name, setName] = useState('');
+    const {
+                register,
+                handleSubmit,
+                formState: { errors },
+    } = useForm({resolver: zodResolver(signupSchema)});
+    
+    const submitHandler = (data) => {
+        console.log(data);
+    }
+    return (
+        <div className='flex justify-center items-center min-h-screen p-4'>
+            <div className='card w-96 shadow-xl bg-base-100'>
+                <div className='card-body'>
+                    <form onSubmit={handleSubmit(submitHandler)}>
+                        <div className='form-control mt-4'>
+                            <label className="label mb-1">
+                                <span className="label-text">First Name</span>
+                            </label>
+                            <input {...register('firstName')} placeholder='Enter your firstName' className='input input-bordered input-md'/>
+                            {errors.firstName && (<span className='text-error'>{errors.firstName.message}</span>)}
+                        </div>
+                        <div className='form-control mt-4'>
+                            <label className='label mb-1'>
+                                <span className='label-text'>Email</span>
+                            </label>
+                            <input {...register('email')} placeholder= 'Enter your Email' className='input input-md'/>
+                            {errors.email && (<span className='text-error'>{errors.email.message}</span>)}
+                        </div>
+                        <div className='form-control mt-4'>
+                            <label className='label mb-1'>
+                                <span className='label-text'>Password</span>
+                            </label>
+                            <input {...register('password')} placeholder= 'Enter your password' type="password" className='input input-md'/>
+                            {errors.password && (<span className='text-error'>{errors.password.message}</span>)}
+                        </div>
+                        <div className='flex justify-center mt-4'>
+                            <button type="submit" className='btn btn-primary py-1 px-5'>Sign Up</button>
+                        </div>
+                    </form>
+                </div>
+                    
+            </div>
+            
+        </div>
+    )
+}
+export default Signup;
+
+/* const [name, setName] = useState('');
     // const [email, setEmail] = useState('');
     // const [password, setPassword] = useState('');
      /*    validation (Frontend mein validation krenge taaki server pe baar baar call naa krna padhe aur uspe load naa ho aur slow bhi hogaa)
@@ -11,33 +74,15 @@ function Signup(){
     // const submitHandler= (e) =>{ 
     //     e.preventDefault();
     //     console.log(name,email,password);
-    // }
+    // } */
 
-    const {
-                register,
-                handleSubmit,
-                formState: { errors },
-    } = useForm();
 
-    const submitHandler = (data) => {
-        console.log(data);
-    }
-    return (
-       
-        // <form onSubmit={submitHandler}>
-        //     <input type='text' placeholder="Enter your name" value={name} onChange={()=> setName(value)}></input>
-        //     <input type='email' placeholder="Enter your email" value={email} onChange={()=> setEmail(value)}></input>
-        //     <input type='password' placeholder="Enter your password" value={password} onChange={()=> setPassword(value)}></input>
-        // </form>
 
-        // { using react hook form}
-        //  Yahha pe 'firstName' property h value nhii 
-        <form onSubmit={handleSubmit(submitHandler)} className="flex flex-col justify-center align-items">
-            <input {...register('firstName')} placeholder='Enter your firstName'/>
-            <input {...register('email')} placeholder= 'Enter your Email'/>
-            <input {...register('password')} placeholder= 'Enter your password'/>
-            <button type="submit">Submit</button>
+    /*<form onSubmit={submitHandler}>
+             <input type='text' placeholder="Enter your name" value={name} onChange={()=> setName(value)}></input>
+             <input type='email' placeholder="Enter your email" value={email} onChange={()=> setEmail(value)}></input>
+             <input type='password' placeholder="Enter your password" value={password} onChange={()=> setPassword(value)}></input>
         </form>
-    )
-}
-export default Signup;
+
+        { using react hook form}
+      Yahha pe 'firstName' property h value nhii */
